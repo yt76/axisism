ee67fea99e332a6d4bf9202999e406edc89d19fe
Yusuke Tabata <tabata.yusuke@gmail.com>
2020-12-13 21:55:20 +0900

    read handler.

--- axi_tb.cpp
+++ axi_tb.cpp
@@ -7,7 +7,24 @@ void AxiTB::writeHandler() {
 }
 
 void AxiTB::readHandler() {
-  while (true) {
+  wait(clk.posedge_event());
+  while (!m_ARVALID.read()) {
     wait(clk.posedge_event());
   }
+  m_ARREADY.write(true);
+  wait(clk.posedge_event());
+  m_ARREADY.write(false);
+  int len = m_ARLEN.read() + 1;
+  m_RVALID.write(true);
+  wait(clk.posedge_event());
+  for (int i = 0; i < len; ++i) {
+    while (!m_RREADY.read()) {
+      wait(clk.posedge_event());
+    }
+    wait(clk.posedge_event());
+    if (i == len - 1) {
+      m_RLAST.write(true);
+    }
+  }
+  m_RLAST.write(false);
 }
